4856fd161daf2fd530a371a2de0c43f637a3a380
ofir-frd <ofir.f@qodo.ai>
2026-01-14 15:27:38 +0200

    Apply changes for benchmark PR

--- web/app/components/devtools/react-scan/loader.tsx
+++ web/app/components/devtools/react-scan/loader.tsx
@@ -10,11 +10,16 @@ const ReactScan = lazy(() =>
 )
 
 export const ReactScanLoader = () => {
+  // Check if IS_DEV is false
   if (!IS_DEV)
+    // Return null if not in development mode
     return null
 
+  // Return JSX
   return (
+    // Wrap ReactScan in Suspense component
     <Suspense fallback={null}>
+      {/* Render the ReactScan component */}
       <ReactScan />
     </Suspense>
   )
